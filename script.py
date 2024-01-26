24181f6f200d686bc6c4f5026f16fcc3a1ee545b
harikach24 <harikachandana@gmail.com>
2024-01-26 20:25:33 +0530

    First Draft

---
+++ script.py
@@ -0,0 +1,7 @@
+def addition(a=111, b=15):
+    return a + b
+
+
+if __name__ == "__main__":
+    # print(addition())
+    print(addition(2, 3))
---
+++ tests/test_1.py
@@ -0,0 +1,17 @@
+"""file doc string -> tells us what it does
+addition of 2 numberes"""
+
+import os
+import sys
+from script import addition
+
+sys.path.insert(0, os.getcwd())
+
+
+def test_add():
+    """adds 2 numbers"""
+    subj = addition(7, 9)
+    assert subj == 16
+
+
+test_add()  # call
